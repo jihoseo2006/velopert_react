a124bcb9ec73f20ab317ed4332e1c6b2f0dc2838
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2023-12-03 23:48:03 +0900

    20
    20. useReducer 를 사용하여 상태 업데이트 로직 분리하기

--- begin-react/src/Counter.js
+++ begin-react/src/Counter.js
@@ -1,16 +1,28 @@
 //7. useState를 통해 컴포넌트에서 바뀌는 값 관리하기
+//20. useReducer 를 사용하여 상태 업데이트 로직 분리하기
+import React, { useReducer, useState } from "react";
 
-import React, { useState } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
 
 function Counter(){
 
-  const [number, setNumber] = useState(0)
+  const [number, dispatch] = useReducer(reducer, 0);
 
   const onIncrease = () => {
-    setNumber(prevNumber => prevNumber + 1);
+    dispatch({ type: 'INCREMENT'});
   }
   const onDecrease = () => {
-    setNumber(prevNumber => prevNumber - 1);
+    dispatch({ type: 'DECREMENT'});
   }
 
   return (
