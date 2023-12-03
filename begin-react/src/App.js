ed075325d01da23b4625db2d652625e7f3b1d9ee
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2023-12-03 22:25:21 +0900

    12~16
    useEffect를 사용하여 마운트/언마운트/업데이트시 할 작업 설정하기

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,12 +1,94 @@
-import React from 'react';
-import Counter from './Counter';
-import InputSample from './InputSample';
+import React, {useRef, useState} from 'react';
 import UserList from './UserLIst';
+import CreateUser from './CreateUser';
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username:'',
+    email: ''
+  });
+
+  const {username  , email} = inputs;
+  const onChange = e => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  }
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false
+    }
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    //spread 연산자를 사용하여 불변성을 지키며 배열에 새항목을 추가하는 방법
+    // setUsers([...users, user]);
+    
+    //concat 함수를 사용하여 새로운 원소가 추가된 새로운 배열을 만드는 방법
+    setUsers(users.concat(user));
+
+    setInputs({
+      username:'',
+      email:''
+    });
+    nextId.current += 1;
+  }
+
+  const onRemove = id => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+    setUsers(users.filter(user => user.id !== id));
+  }
+
+  const onToggle = id => {
+    setUsers(
+      users.map(user => 
+        // id 값을 비교해서 id 값이 다르면 그대로 두고, 같다면 active 값을 반전시킨다.
+        user.id === id ? {...user, active: !user.active } : user
+      )
+    );
+  }
+
   return (
-    <UserList />
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList 
+        users={users} 
+        onRemove={onRemove} 
+        onToggle={onToggle}
+      />
+    </>
   );
 }
 
 export default App;
+
--- begin-react/src/UserLIst.js
+++ begin-react/src/UserLIst.js
@@ -1,39 +1,48 @@
 //11. 배열 렌더링하기
+//12. useRef 로 컴포넌트 안의 변수 만들기
+//13. 배열에 항목 추가하기
+//14. 배열에 항목 제거하기
+//15. 배열에 항목 수정하기
+//16. useEffect를 사용하여 마운트/언마운트/업데이트시 할 작업 설정하기
+import React, { useEffect } from "react";
 
-import React from "react";
-
-function User({ user }) {
+function User({ user, onRemove, onToggle }) {
+  useEffect(() => {
+    console.log('컴포넌트가 화면에 나타남')
+    return () => {
+      console.log('컴포넌트가 화면에서 사라짐');
+    };
+  },[]);
+  //onRemove가 "id가 _인 객체를 삭제해라" 라는 역할을 가지고 있음.
   return (
     <div>
-      <b>{user.username}</b> <span>({user.email})</span>
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black'
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
 }
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com'
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com'
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com'
-    }
-  ];
-
+function UserList({ users, onRemove,onToggle }) {
   return (
     <div>
       {users.map(user => (
-        <User user={user} key={user.id}/>
-        ))}
+        <User 
+          user={user} 
+          key={user.id} 
+          onRemove={onRemove} 
+          onToggle={onToggle}  
+        />
+      ))}
     </div>
   );
 }
