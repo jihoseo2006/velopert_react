f0565b83c9b7bbe5d1e9448c95dd862f49f425d0
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2023-12-05 22:42:48 +0900

    Mashup-todolist
    투두리스트 완성

--- mashup-todolist/src/components/TodoHead.js
+++ mashup-todolist/src/components/TodoHead.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import {useTodoState} from '../TodoContext'
+import { useTodoState } from '../TodoContext'
 
 const TodoHeadBlock = styled.div`
   padding-top: 48px;
@@ -27,12 +27,21 @@ const TodoHeadBlock = styled.div`
 `;
 function TodoHead() {
   const todos = useTodoState();
-  console.log(todos);
+  const undoneTasks = todos.filter(todo => !todo.done);
+
+  const today = new Date();
+  const dateString = today.toLocaleDateString('ko-KR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+  const dayName = today.toLocaleDateString('ko-KR', {weekday: 'long'});
+
   return (
     <TodoHeadBlock>
-      <h1>2023년 12월 5일</h1>
-      <div className="day">화요일</div>
-      <div className="tasks-left">할일 2개 남음</div>
+      <h1>{dateString}</h1>
+      <div className="day">{dayName}</div>
+      <div className="tasks-left">할일 {undoneTasks.length}개 남음</div>
     </TodoHeadBlock>
   );
 }
--- mashup-todolist/src/components/TodoTemplate.js
+++ mashup-todolist/src/components/TodoTemplate.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
+
 const TodoTemplateBlock = styled.div`
   width: 512px;
   height: 768px;
