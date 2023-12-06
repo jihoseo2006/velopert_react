3632bc920c07b7123ddd2f135f50ad4a7a8a2c14
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2023-12-07 08:32:53 +0900

    useAsync custom hooks
    커스텀훅으로 api 연동해서 사용하기

--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -1,70 +1,40 @@
-import React, { useReducer, useEffect } from 'react'
+import React, { useReducer, useEffect, useState } from 'react'
 import axios from 'axios';
+import useAsync from './useAsync';
+import User from './User';
 
-function reducer(state, action) {
-  switch (action.type) {
-    case 'LOADING':
-      return {
-        loading: true,
-        data: null,
-        error: null
-      };
-    case 'SUCCESS':
-      return {
-        loading: false,
-        data: action.data,
-        error: null
-      };
-    case 'ERROR':
-      return {
-        loading: false,
-        data: null,
-        error: action.error
-      };
-    default:
-      throw new Error(`Unhandled action type: ${action.type}`);
-  }
+async function getUsers() {
+  const response = await axios.get(
+    'https://jsonplaceholder.typicode.com/users'
+  );
+  return response.data;
 }
 
 
 function Users() {
-  const [state, dispatch] = useReducer(reducer, {
-    loading: false,
-    data: null,
-    error: null
-  });
-
-  const fetchUsers = async () => {
-    dispatch({type: 'LOADING'});
-    try {
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/users'
-      );
-      dispatch({type: 'SUCCESS', data: response.data});
-    } catch (e) {
-      dispatch({type: 'ERROR', error: e})
-    }
-  }
+  const [userId, setUserId] = useState(null);
+  const [state, refatch] = useAsync(getUsers, []);
 
   const {loading, data: users, error} = state; // state.data 를 users 키워드로 조회
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
   if (loading) return <div>로딩중....</div>;
   if (error) return <div>에러가 발생했습니다</div>;
-  if (!users) return null;
+  if (!users) return <button onClick={refatch}>불러오기</button>;
   return (
     <>
       <ul>
         {users.map(user => (
-          <li key={user.id}>
+          <li 
+            key={user.id}
+            onClick={() => setUserId(user.id)}
+            style={{cursor: 'pointer'}}
+          >
             {user.username} ({user.name})
           </li>
         ))}
       </ul>
-      <button onClick={fetchUsers}>다시 불러오기</button>
+      <button onClick={refatch}>다시 불러오기</button>
+      {userId && <User id={userId} />}
     </>
   );
 }
