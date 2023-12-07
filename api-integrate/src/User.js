a79189c3a4e9d7d4e997dabad8e0cd321a6aff2d
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2023-12-07 10:38:04 +0900

    Using react-async manage response state
    react-async 사용해서 요청상태 관리하기

--- api-integrate/src/User.js
+++ api-integrate/src/User.js
@@ -1,23 +1,20 @@
 import React from 'react'
-import axios from 'axios';
-import useAsync from './useAsync';
+import axios from 'axios'
+import { useAsync } from 'react-use'
 
 async function getUser(id) {
-  const response = await axios.get(
-    `https://jsonplaceholder.typicode.com/users/${id}`
-  );
-  return response.data;
+  const url = `https://jsonplaceholder.typicode.com/users/${id}`
+  const response = await axios.get(url)
+  return response.data
 }
 
-function User({id}) {
-  const [state] = useAsync(() => getUser(id), [id]);
-  const {loading, data: user, error} = state;
+function User({ id }) {
+  const state = useAsync(() => getUser(id), [id])
+  const { loading, error, value: user } = state
 
-
-  if (loading) return <div>로딩중..</div>;
-  if (error) return <div>에러가 발생했습니다</div>;
-  if (!user) return null;
-
+  if (loading) return <div>로딩중..</div>
+  if (error) return <div>Error Occured: {error.message}</div>
+  if (!user) return null
   return (
     <div>
       <h2>{user.username}</h2>
@@ -28,4 +25,4 @@ function User({id}) {
   )
 }
 
-export default User;
+export default User
--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -1,42 +1,40 @@
 import React, { useReducer, useEffect, useState } from 'react'
 import axios from 'axios';
-import useAsync from './useAsync';
+import { useAsyncRetry } from 'react-use';
 import User from './User';
 
 async function getUsers() {
-  const response = await axios.get(
-    'https://jsonplaceholder.typicode.com/users'
-  );
-  return response.data;
+  const url = 'https://jsonplaceholder.typicode.com/users'
+  const response = await axios.get(url)
+  return response.data
 }
 
 
-function Users() {
-  const [userId, setUserId] = useState(null);
-  const [state, refatch] = useAsync(getUsers, []);
+const Users = () => {
+  const [userId, setUserId] = useState(null)
+  const state = useAsyncRetry(getUsers)
+  const { loading, error, value: users, retry } = state
 
-  const {loading, data: users, error} = state; // state.data 를 users 키워드로 조회
-
-  if (loading) return <div>로딩중....</div>;
-  if (error) return <div>에러가 발생했습니다</div>;
-  if (!users) return <button onClick={refatch}>불러오기</button>;
+  if (loading) return <div>로딩중..</div>
+  if (error) return <div>Error Occured: {error.message}</div>
+  if (!users) return <button onClick={retry}>불러오기</button>
   return (
     <>
       <ul>
-        {users.map(user => (
-          <li 
+        {users.map((user) => (
+          <li
             key={user.id}
             onClick={() => setUserId(user.id)}
-            style={{cursor: 'pointer'}}
+            style={{ cursor: 'pointer' }}
           >
             {user.username} ({user.name})
           </li>
         ))}
       </ul>
-      <button onClick={refatch}>다시 불러오기</button>
+      <button onClick={retry}>again</button>
       {userId && <User id={userId} />}
     </>
-  );
+  )
 }
 
-export default Users;
+export default Users
